9eb0a8f3ca308be370ce97ffe430e1f399d8c903
ChristianBelair <christianbelair@cmail.carleton.ca>
2021-03-01 11:18:30 -0500

    Added VideoTest public member functions

--- src/PedTracking/PedTracking.hpp
+++ src/PedTracking/PedTracking.hpp
@@ -3,6 +3,12 @@
 
 #include <opencv2/core/utility.hpp>
 
-
+namespace ass {
+    class PedTracking {
+        public:
+            void VideoTest();
+            void VideoTest(int cameraNumber);
+    };
+}
 
 #endif // ndef PEDTRACKING_H

fedf2268fa6fa241f308f0d2196baf7976fe7684
ChristianBelair <christianbelair@cmail.carleton.ca>
2021-03-08 11:09:53 -0500

    Added TrackPeds to test code

--- src/PedTracking/PedTrackingTest.cpp
+++ src/PedTracking/PedTrackingTest.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv) {
     }
 
     if (vFile == "") {
-        pedTracker.VideoTest();
+        pedTracker.TrackPeds();
     }
     else {
         pedTracker.VideoTest(vFile);
